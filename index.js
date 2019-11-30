51103baef2b1028a856fe7e08851046dbed3c003
Terrence Kuiper <github@terrencekuiper.com>
2019-11-30 23:26:48 +0000

    Update port assignment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // app.listen(8080);
-server.listen("8080", () => {
+server.listen(process.env.PORT || 8080, () => {
   console.log("Server listening on Port 8080");
 });
 
@@ -26,14 +26,14 @@ const determineWinner = function(winners) {
   let winningPicture = null;
 
   for (const winner of Object.keys(winners)) {
-    if (winner !== 'total') {
+    if (winner !== "total") {
       if (winners[winner] > winningValue) {
         winningValue = winners[winner];
-        winningPicture = winner
+        winningPicture = winner;
       }
     }
   }
-  console.log(winningPicture)
+  console.log(winningPicture);
   return winningPicture;
 };
 
@@ -56,55 +56,64 @@ app.get("/about", function(req, res) {
 const usernames = {};
 
 // rooms which are currently available in chat
-const rooms = ['Lobby','Arena #1','Arena #2'];
+const rooms = ["Lobby", "Arena #1", "Arena #2"];
 const roomImages = {};
 const roomVotes = {};
 
 // random image
 let img = randomImage();
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function(socket) {
+  // when the client emits 'adduser', this listens and executes
+  socket.on("adduser", function(username) {
+    // store the username in the socket session for this client
+    socket.username = username;
+    // store the room name in the socket session for this client
+    socket.room = "Lobby";
+    // add the client's username to the global list
+    usernames[username] = username;
+    // send client to room 1
+    socket.join("Lobby");
+    // echo to client they've connected
+    socket.emit("updatechat", "SERVER", "you have connected to Lobby");
+    // echo to room 1 that a person has connected to their room
+    socket.broadcast
+      .to("Lobby")
+      .emit("updatechat", "SERVER", username + " has connected to this room");
+    socket.emit("updaterooms", rooms, "Lobby");
+  });
 
-	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(username){
-		// store the username in the socket session for this client
-		socket.username = username;
-		// store the room name in the socket session for this client
-		socket.room = 'Lobby';
-		// add the client's username to the global list
-		usernames[username] = username;
-		// send client to room 1
-		socket.join('Lobby');
-		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
-		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
-		socket.emit('updaterooms', rooms, 'Lobby');
-	});
+  // when the client emits 'sendchat', this listens and executes
+  socket.on("sendchat", function(data) {
+    // we tell the client to execute 'updatechat' with 2 parameters
+    io.sockets.in(socket.room).emit("updatechat", socket.username, data);
+  });
 
-	// when the client emits 'sendchat', this listens and executes
-	socket.on('sendchat', function (data) {
-		// we tell the client to execute 'updatechat' with 2 parameters
-		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
-	});
-
-	socket.on('switchRoom', function(newroom){
+  socket.on("switchRoom", function(newroom) {
     // leave the current room (stored in session)
     var room = io.sockets.adapter.rooms[newroom];
-    if (room === undefined && newroom !== 'Lobby') {
-      let stockImage = randomImage()
-      roomImages[newroom] = {reference: stockImage};
-      roomVotes[newroom] = {total: 0};
+    if (room === undefined && newroom !== "Lobby") {
+      let stockImage = randomImage();
+      roomImages[newroom] = { reference: stockImage };
+      roomVotes[newroom] = { total: 0 };
       socket.leave(socket.room);
       // join new room, received as function parameter
       socket.join(newroom);
-      socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+      socket.emit("updatechat", "SERVER", "you have connected to " + newroom);
       // sent message to OLD room
-      socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+      socket.broadcast
+        .to(socket.room)
+        .emit("updatechat", "SERVER", socket.username + " has left this room");
       // update socket session room title
       socket.room = newroom;
-      socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-      socket.emit('updaterooms', rooms, newroom);
+      socket.broadcast
+        .to(newroom)
+        .emit(
+          "updatechat",
+          "SERVER",
+          socket.username + " has joined this room"
+        );
+      socket.emit("updaterooms", rooms, newroom);
     } else if (newroom !== "Lobby" && room.length < 4) {
       socket.leave(socket.room);
       // join new room, received as function parameter
@@ -112,64 +121,93 @@ io.sockets.on('connection', function (socket) {
       // check how many people are in the room after a person joins
       room = io.sockets.adapter.rooms[newroom];
       if (room.length === 4) {
-        io.in(newroom).emit('displayreference', roomImages[newroom]);
+        io.in(newroom).emit("displayreference", roomImages[newroom]);
       }
 
-      socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+      socket.emit("updatechat", "SERVER", "you have connected to " + newroom);
       // sent message to OLD room
-      socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+      socket.broadcast
+        .to(socket.room)
+        .emit("updatechat", "SERVER", socket.username + " has left this room");
       // update socket session room title
       socket.room = newroom;
-      socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-      socket.emit('updaterooms', rooms, newroom);
+      socket.broadcast
+        .to(newroom)
+        .emit(
+          "updatechat",
+          "SERVER",
+          socket.username + " has joined this room"
+        );
+      socket.emit("updaterooms", rooms, newroom);
     } else if (newroom === "Lobby") {
       socket.leave(socket.room);
       // join new room, received as function parameter
       socket.join(newroom);
-      socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+      socket.emit("updatechat", "SERVER", "you have connected to " + newroom);
       // sent message to OLD room
-      socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+      socket.broadcast
+        .to(socket.room)
+        .emit("updatechat", "SERVER", socket.username + " has left this room");
       // update socket session room title
       socket.room = newroom;
-      socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-      socket.emit('updaterooms', rooms, newroom);
+      socket.broadcast
+        .to(newroom)
+        .emit(
+          "updatechat",
+          "SERVER",
+          socket.username + " has joined this room"
+        );
+      socket.emit("updaterooms", rooms, newroom);
     } else {
-      console.log("FULL ROOM")
+      console.log("FULL ROOM");
     }
   });
-  
-  socket.on('donedrawing', function(drawing) {
+
+  socket.on("donedrawing", function(drawing) {
     roomImages[socket.room][socket.username] = drawing;
     // console.log(roomImages[socket.room][socket.username]);
-    socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has completed their painting');
+    socket.broadcast
+      .to(socket.room)
+      .emit(
+        "updatechat",
+        "SERVER",
+        socket.username + " has completed their painting"
+      );
     if (Object.keys(roomImages[socket.room]).length === 5) {
       for (const user of Object.keys(roomImages[socket.room])) {
-        if (user !== 'reference') {
+        if (user !== "reference") {
           roomVotes[socket.room][roomImages[socket.room][user]] = 0;
         }
       }
       console.log(roomVotes);
-      io.in(socket.room).emit('displayphotos', roomImages[socket.room]);
+      io.in(socket.room).emit("displayphotos", roomImages[socket.room]);
     }
-  })
+  });
 
-  socket.on('submitvote', function(key) {
+  socket.on("submitvote", function(key) {
     roomVotes[socket.room].total += 1;
     roomVotes[socket.room][key] += 1;
     if (roomVotes[socket.room].total === 4) {
-      console.log("WE HAVE A WINNER")
-      io.in(socket.room).emit('displaywinner', determineWinner(roomVotes[socket.room]));
+      console.log("WE HAVE A WINNER");
+      io.in(socket.room).emit(
+        "displaywinner",
+        determineWinner(roomVotes[socket.room])
+      );
     }
-  })
+  });
 
-	// when the user disconnects.. perform this
-	socket.on('disconnect', function(){
-		// remove the username from global usernames list
-		delete usernames[socket.username];
-		// update list of users in chat, client-side
-		io.sockets.emit('updateusers', usernames);
-		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-		socket.leave(socket.room);
-	});
+  // when the user disconnects.. perform this
+  socket.on("disconnect", function() {
+    // remove the username from global usernames list
+    delete usernames[socket.username];
+    // update list of users in chat, client-side
+    io.sockets.emit("updateusers", usernames);
+    // echo globally that this client has left
+    socket.broadcast.emit(
+      "updatechat",
+      "SERVER",
+      socket.username + " has disconnected"
+    );
+    socket.leave(socket.room);
+  });
 });
