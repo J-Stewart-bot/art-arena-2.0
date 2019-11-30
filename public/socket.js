0571c15485e22624a11fe1ac840ac159ed4a227f
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-11-30 22:41:05 +0000

    Add the ability to vote on your favourite image, and view the winning image

--- public/socket.js
+++ public/socket.js
@@ -32,7 +32,7 @@ socket.on("updaterooms", function(rooms, current_room) {
 socket.on("displayphotos", function(images) {
   let i = 1;
   for (const image of Object.keys(images)) {
-    if (image !== "newroom") {
+    if (image !== "reference") {
       $(`#spot${i}`).attr("src", images[image]);
       i++;
     }
@@ -42,6 +42,10 @@ socket.on("displayphotos", function(images) {
 socket.on("displayreference", function(images) {
   $("#mainImage").attr("src", images.reference);
 });
+
+socket.on('displaywinner', function(image) {
+  $("#mainImage").attr("src", image);
+})
 
 function switchRoom(room) {
   socket.emit("switchRoom", room);
@@ -60,6 +64,7 @@ $(function() {
   // when the client clicks submit
   $("#drawing-complete").click(function() {
     //when the client clicks SUBMIT
+    $('#toRemove').css('visibility','hidden');
     $(this).attr("disabled", "disabled");
     var canvas = document.getElementById("my-canvas");
     var dataURL = canvas.toDataURL();
@@ -76,4 +81,37 @@ $(function() {
         .click();
     }
   });
+
+  $('#vote1').click(function() {
+    $(this).attr("disabled", "disabled");
+    $('#vote2').attr("disabled", "disabled");
+    $('#vote3').attr("disabled", "disabled");
+    $('#vote4').attr("disabled", "disabled");
+    let voteFor = document.getElementById('spot1').src;
+    socket.emit('submitvote', voteFor);
+  })
+  $('#vote2').click(function() {
+    $(this).attr("disabled", "disabled");
+    $('#vote1').attr("disabled", "disabled");
+    $('#vote3').attr("disabled", "disabled");
+    $('#vote4').attr("disabled", "disabled");
+    let voteFor = document.getElementById('spot2').src;
+    socket.emit('submitvote', voteFor);
+  })
+  $('#vote3').click(function() {
+    $(this).attr("disabled", "disabled");
+    $('#vote2').attr("disabled", "disabled");
+    $('#vote1').attr("disabled", "disabled");
+    $('#vote4').attr("disabled", "disabled");
+    let voteFor = document.getElementById('spot3').src;
+    socket.emit('submitvote', voteFor);
+  })
+  $('#vote4').click(function() {
+    $(this).attr("disabled", "disabled");
+    $('#vote2').attr("disabled", "disabled");
+    $('#vote3').attr("disabled", "disabled");
+    $('#vote1').attr("disabled", "disabled");
+    let voteFor = document.getElementById('spot4').src;
+    socket.emit('submitvote', voteFor);
+  })
 });
