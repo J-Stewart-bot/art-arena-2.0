28e9c14e6b0993e2130c169e0dfaa089352fef0f
Terrence Kuiper <github@terrencekuiper.com>
2019-12-03 05:38:32 +0000

    Add firebase cloud storage

--- public/canvas.js
+++ public/canvas.js
@@ -145,6 +145,73 @@ controls.save = function(cx) {
   return link;
 };
 
+controls.cloudSave = function(cx) {
+  let link = document.createElement("a");
+  link.innerHTML = "cloudSave";
+  link.addEventListener(
+    "click",
+    function() {
+      const file = cx.canvas.toDataURL();
+      const randomNum = Math.random() * 100000000000000000;
+
+      // Create the file metadata
+      const metadata = {
+        contentType: "data_url"
+      };
+
+      // Upload file and metadata to the object 'images/mountains.jpg'
+      let uploadTask = storageRef
+        .child("images/" + randomNum.toString())
+        .putString(file, "data_url");
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function(snapshot) {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          var progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log("Upload is paused");
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log("Upload is running");
+              break;
+          }
+        },
+        function(error) {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        },
+        function() {
+          // Upload completed successfully, now we can get the download URL
+          uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+            console.log("File available at", downloadURL);
+          });
+        }
+      );
+    },
+    false
+  );
+  document.body.appendChild(link);
+  return link;
+};
+
 controls.reset = function(cx) {
   var link = elt("button", { type: "button", onclick: "reset()" }, "Reset");
   return link;
